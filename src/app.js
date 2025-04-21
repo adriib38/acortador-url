01833fa7a353c19fa8af645cb31a9ac2625a16d2
adriib38 <adrianbenitezb7@gmail.com>
2025-04-21 20:43:08 +0200

    Add rate limit

--- src/app.js
+++ src/app.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const logger = require('morgan');
 const cookieParser = require("cookie-parser");
-
 const app = express();
+const rateLimit = require('express-rate-limit');
 
 const allowedOrigin = process.env.ALLOWED_ORIGIN || 'http://localhost:3000';
 
 app.use(cookieParser());
+const limiter = rateLimit({
+	windowMs: 60 * 1000, //1minute
+	limit: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false,
+  message: 'Too many requests, please try again later.',
+
+})
+
+app.use(limiter); 
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', allowedOrigin);
